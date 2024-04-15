8c4db70a2244148436ad772279e2661d6f4c2f02
margareth91 <margerytka@wp.eu>
2024-04-15 17:01:04 +0200

    added model meal for planner app

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
