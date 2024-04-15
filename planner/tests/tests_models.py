6074a86ffbf6725a855a9e5c33e6544247fa75dc
margareth91 <margerytka@wp.eu>
2024-04-15 17:29:35 +0200

    added tests for model meal

---
+++ planner/tests/tests_models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from planner.models import Meal
+
+
+class MealModelTest(TestCase):
+    MEAL_NAME = "Meal"
+    MEAL_DESCRIPTION = "Meal description"
+
+    def setUp(self):
+        self.author = User.objects.create()
+        Meal.objects.create(
+            author=self.author, name=self.MEAL_NAME, description=self.MEAL_DESCRIPTION
+        )
+        self.meal = Meal.objects.get(id=1)
+
+    def test_meal_author(self):
+        self.assertTrue(self.meal.author, self.author)
+
+    def test_meal_name(self):
+        self.assertEqual(self.meal.name, self.MEAL_NAME)
+
+    def test_meal_description(self):
+        self.assertEqual(self.meal.description, self.MEAL_DESCRIPTION)
+
+    def test_str(self):
+        self.assertEqual(self.meal.__str__(), self.meal.name)
