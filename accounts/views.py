261b3d5181f5ee54d3c2c8b6353436382871ea0f
margareth91 <margerytka@wp.eu>
2024-04-17 17:54:46 +0200

    added user sign up view to accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from accounts.serializers import UserSignUpSerializer
+
+
+class UserSignUpView(generics.GenericAPIView):
+    serializer_class = UserSignUpSerializer
+
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {"message": "User created successfully", "data": serializer.data}
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
