ef3d05a078ea99fa1f875b21e33c3a9e1ca52f59
margareth91 <margerytka@wp.eu>
2024-04-18 13:52:38 +0200

    added login urls to accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
-from . import views
 from django.urls import path
+
+from . import views
 
 urlpatterns = [
     path("signup/", views.UserSignUpView.as_view(), name="signup"),
+    path("login/", views.UserLoginView.as_view(), name="login"),
 ]
