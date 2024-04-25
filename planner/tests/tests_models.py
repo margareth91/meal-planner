7431ff1590fc3d6eb1e4f9504185c7ae1c6c74f2
margareth91 <margerytka@wp.eu>
2024-04-25 15:02:23 +0200

    changed to a custom user model in meal model tests

--- planner/tests/tests_models.py
+++ planner/tests/tests_models.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from planner.models import Meal
+
+User = get_user_model()
 
 
 class MealModelTest(TestCase):
