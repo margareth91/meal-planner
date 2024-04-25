d45055eb90b9ffe4b548251385388c419bc78cb8
margareth91 <margerytka@wp.eu>
2024-04-25 14:57:54 +0200

    changed to a custom user model in meal model

--- planner/models.py
+++ planner/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 
 class Meal(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
 
