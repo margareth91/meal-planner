b702f133dc0e2feae5d1c6e25d4f4a6f50218903
margareth91 <margerytka@wp.eu>
2024-04-26 13:05:08 +0200

    added weekday field with choices to model meal

--- planner/models.py
+++ planner/models.py
@@ -1,11 +1,22 @@
 from django.conf import settings
 from django.db import models
 
+DAY_OF_THE_WEEK = [
+    ("1", "Monday"),
+    ("2", "Tuesday"),
+    ("3", "Wednesday"),
+    ("4", "Thursday"),
+    ("5", "Friday"),
+    ("6", "Saturday"),
+    ("7", "Sunday"),
+]
+
 
 class Meal(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
+    weekday = models.CharField(choices=DAY_OF_THE_WEEK, max_length=10)
 
     def __str__(self):
         return self.name
