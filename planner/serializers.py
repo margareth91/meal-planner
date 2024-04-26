e749cb11c27dc1678394f185f16d5960a15bc8b8
margareth91 <margerytka@wp.eu>
2024-04-26 13:07:34 +0200

    added weekday field to meal serializer fields

--- planner/serializers.py
+++ planner/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+
 from planner.models import Meal
 
 
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
-        fields = ["id", "author", "name", "description"]
+        fields = ["id", "author", "weekday", "name", "description"]
